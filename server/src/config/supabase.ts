4f5cbbafd8d789a2e2f7d37862a789c7da4de912
atik806 <atikrj220@gmail.com>
2026-08-21 10:39:41 +0600

    added db config

---
+++ server/src/config/supabase.ts
@@ -0,0 +1,19 @@
+import { createClient } from '@supabase/supabase-js';
+import { config } from 'dotenv';
+
+config();
+
+
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
+
+if(!supabaseUrl || !supabaseServiceRoleKey) {
+    throw new Error('Supabase URL and Service Role Key must be provided in the environment variables.');
+}
+
+
+export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
+
+
+
+
