6be4ccfc7a4965e93f4bb067ac11c84b5ccbaf39
atik806 <atikrj220@gmail.com>
2026-08-21 10:40:40 +0600

    added transaction controller:

---
+++ server/src/controllers/transaction.controller.ts
@@ -0,0 +1,15 @@
+
+
+
+
+//get all the transactions 
+
+export const getAllTransactions = async (req: Request, res: Response) => {
+    try{
+
+    }catch(error) {
+        console.error('Error fetching transactions:', error);
+ //       res.status(500).json({ message: 'Failed to fetch transactions' });
+    }
+}
+
