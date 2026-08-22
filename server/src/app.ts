8c740965c9467e01fe18307a4400142fba8c2079
atik806 <atikrj220@gmail.com>
2026-08-22 12:03:42 +0600

    update the app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,28 @@
 import cors = require('cors');
 import express = require('express');
+import farmerRoutes from "./routes/farmer.routes";
+
+
 
 
 
 const app = express();
-
-app.use(cors());
+//using middeware to allow cross-origin requests from any origin
+app.use(cors({
+    origin: '*',
+}));
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello Giganigga' });
+//Health check
+
+app.get('/',(req, res) => {
+    res.status(200).json({ message: 'Sofol api is running' });
 });
 
+//farmer api
+
+app.use('/api/farmer', farmerRoutes);
 
 
 export = app;
