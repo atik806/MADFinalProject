eddf1c86315bfd1a16e9b4f800f3719cce8bdd43
atik806 <atikrj220@gmail.com>
2026-08-22 12:03:21 +0600

    update the transaction router

--- server/src/routes/tranaction.routes.ts
+++ server/src/routes/tranaction.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getTransactionById } from '../controllers/transaction.controller';
+import { createTransaction, deleteTransaction, getAllTransactions, getTransactionById, updateTransaction } from '../controllers/transaction.controller';
 import { authenticateUser } from '../middleware/auth.middleware';
 import { farmerOnly } from '../middleware/role.middleware';
 
@@ -7,7 +7,15 @@ import { farmerOnly } from '../middleware/role.middleware';
 const router = Router();
 router.use(authenticateUser);
 router.use(farmerOnly);
-
+router.get("/", getAllTransactions);
 router.get("/id:", getTransactionById);
+
+router.post("/", createTransaction);
+
+router.put("/:id", updateTransaction);
+
+router.delete("/:id", deleteTransaction);
+export default router;
+
 
 
